418192c0abea1d8b5da12e487578da028682e145
renjy <86453847@qq.com>
2019-08-11 01:16:17 +0800

    分页功能

--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -19,9 +19,9 @@
           <el-select v-model="reqParams.channel_id" placeholder="请选择">
             <el-option
               v-for="item in channelOptions"
-              :key="item.value"
-              :label="item.label"
-              :value="item.value"
+              :key="item.id"
+              :label="item.name"
+              :value="item.id"
             ></el-option>
           </el-select>
         </el-form-item>
@@ -38,20 +38,60 @@
           <el-button type="primary">筛选</el-button>
         </el-form-item>
       </el-form>
+      <source />
     </el-card>
     <!-- 结果区域 -->
-    <my-text>
-      <!-- <div slot="content" slot-scope="scope">我是： {{scope.msg}}</div> -->
-      <template v-slot:content='scope'>我是： {{scope.msg}}</template>
-      <div slot="footer">footer</div>
-    </my-text>
+    <el-card>
+      <div slot="header">根据筛选条件共查询到 {{total}} 条结果：</div>
+      <!-- 表格組件 -->
+      <el-table :data="articles" style="width: 100%">
+        <el-table-column label="封面">
+          <template slot-scope="scope">
+            <el-image :src="scope.row.cover.images[0]" fit="cover" style="width:120px;height:80px">
+              <div slot="error" class="image-slot">
+                <img src="../../assets/images/error.gif" alt style="width:120px;height:80px" />
+              </div>
+            </el-image>
+          </template>
+        </el-table-column>
+        <el-table-column label="标题" prop="title"></el-table-column>
+        <el-table-column label="状态">
+          <template slot-scope="scope">
+            <el-tag v-if="scope.row.status === 0" type="info">草稿</el-tag>
+            <el-tag v-if="scope.row.status === 1">待審核</el-tag>
+            <el-tag v-if="scope.row.status === 2" type="success">審核通過</el-tag>
+            <el-tag v-if="scope.row.status === 3" type="warning">審核失敗</el-tag>
+            <el-tag v-if="scope.row.status === 4" type="danger">已刪除</el-tag>
+          </template>
+        </el-table-column>
+        <el-table-column label="发布时间" prop="pubdate"></el-table-column>
+        <el-table-column label="操作">
+          <template slot-scope="scope">
+            <el-button type="primary" icon="el-icon-edit" plain circle></el-button>
+            <el-button type="danger" @click="del(scope.row.id)" icon="el-icon-delete" plain circle></el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+      <!-- 分页组件 -->
+      <div style="text-align:center;margin-top:30px;">
+        <!-- 分页组件  默认的每页显示10条数   page-size 每页显示几条-->
+        <!-- 当你更改组件的当前页码的时候  拿着新的页码重新请求渲染列表  @current-change="changePager"-->
+        <!-- 当你不是点击按钮触发的分页 不会去选中按钮的样式  current-page 需要把当前页码数据和分页组件进行绑定 -->
+        <el-pagination
+          background
+          layout="prev, pager, next,total"
+          :total="total"
+          :page-size="reqParams.per_page"
+          :current-page="reqParams.page"
+          @current-change="changePager"
+        ></el-pagination>
+      </div>
+    </el-card>
   </div>
 </template>
 
 <script>
-
 export default {
-
   data () {
     return {
       // 收集请求的参数
@@ -59,15 +99,53 @@ export default {
         status: null,
         channel_id: null,
         begin_pubdate: null,
-        end_pubdate: null
+        end_pubdate: null,
+        page: 1,
+        per_page: 20
       },
       // 频道下拉菜单数据
       channelOptions: [],
       // 日期数据
-      dateArr: []
+      dateArr: [],
+      // 文章数据
+      articles: [],
+      // 总数
+      total: 0
+    }
+  },
+  created () {
+    // 获取下来菜单信息
+    this.getChannelOptions()
+    // 获取文章列表数据
+    this.getArticles()
+  },
+  methods: {
+    // 点击当前页 获取新的数据
+    changePager (newPager) {
+      this.reqParams.page = newPager
+      this.getArticles()
+    },
+    async getChannelOptions () {
+      const {
+        data: { data }
+      } = await this.$http.get('channels')
+      this.channelOptions = data.channels
+    },
+    async getArticles () {
+      // 请求方式是 get 传递参数为对象 {params：需要传递的参数}
+      const {
+        data: { data }
+      } = await this.$http.get('/articles', { params: this.reqParams })
+      this.articles = data.results
+      // 获取总条数
+      this.total = data.total_count
     }
   }
 }
 </script>
 
-<style scoped lang='less'></style>
+<style scoped lang='less'>
+.el-card {
+  margin-bottom: 20px;
+}
+</style>
